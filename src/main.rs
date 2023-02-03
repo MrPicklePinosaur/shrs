d8693a4f3936c0b7a05a6b00555193afa6ced1b6
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-03 00:00:50 -0500

    (master) basic shell functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
-#![allow(unused)]
-#![allow(dead_code)]
+use std::{
+    io::{stdin, stdout, Write},
+    process::Command,
+};
 
 fn main() {
-    println!("こんにちは世界！");
+    loop {
+        print!("> ");
+        stdout().flush();
+
+        let mut line = String::new();
+        if let Err(e) = stdin().read_line(&mut line) {
+            continue;
+        }
+
+        let mut parts = line.trim().split_whitespace();
+        let cmd = parts.next().unwrap();
+        let args = parts.collect();
+
+        run_command(cmd, &args);
+    }
+}
+
+fn run_command(cmd: &str, args: &Vec<&str>) {
+    match cmd {
+        "cd" => run_cd_command(args),
+        "exit" => run_exit_command(args),
+        _ => run_external_command(cmd, args),
+    };
+}
+
+fn run_cd_command(args: &Vec<&str>) {}
+
+fn run_exit_command(args: &Vec<&str>) {
+    std::process::exit(0);
+}
+
+fn run_external_command(cmd: &str, args: &Vec<&str>) {
+    let child = Command::new(cmd).args(args).spawn();
+
+    match child {
+        Ok(mut child) => {
+            child.wait();
+        },
+        Err(e) => {
+            eprintln!("{}", e);
+        },
+    };
 }

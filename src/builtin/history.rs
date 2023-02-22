5b268a4f201076d4e2513bd381357faea8a5fe6f
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-22 02:58:27 -0500

    use pino argparse

--- src/builtin/history.rs
+++ src/builtin/history.rs
@@ -1,17 +1,35 @@
 //! The shell builtin that wraps functionality of the History module
 
+// debatable if crate::history should be moved to crate::builtin::history
+
 use std::{
     io::{stdout, Write},
     process::Child,
 };
 
+use pino_argparse::{Cli, Command, FlagParse};
+
 use crate::shell::{dummy_child, Context, Shell};
 
 pub fn history_builtin(ctx: &mut Context, args: &Vec<String>) -> anyhow::Result<Child> {
-    let history = ctx.history.all();
-    for (i, h) in history.iter().enumerate() {
-        print!("{} {}", i, h);
-    }
-    stdout().flush()?;
+    let cli = Cli {
+        program_name: "history",
+        synopsis: "query and modify shell history",
+        root_command: Command {
+            flags: vec![],
+            handler: move |flagparse: FlagParse| -> Result<(), Box<dyn std::error::Error>> {
+                // let history = ctx.history.all();
+                // for (i, h) in history.iter().enumerate() {
+                //     print!("{} {}", i, h);
+                // }
+                // stdout().flush()?;
+                Ok(())
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    cli.run(args);
     dummy_child()
 }
--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -1,1 +1,9 @@
 pub mod history;
+
+use std::process::Child;
+
+use crate::shell::Context;
+
+// trait Builtin {
+//     fn run(ctx: &mut Context, args: &Vec<String>) -> anyhow::Result<Child>;
+// }

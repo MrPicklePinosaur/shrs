a6498f088160cce9be716ad9e1539ef9372c2dd0
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-26 17:27:45 -0500

    single quote, double quote, word lexer impl

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+// heavily inspired by https://github.com/nixpulvis/oursh/blob/develop/src/program/posix/lex.rs
+
 use std::str::CharIndices;
 
 pub type Spanned<Token, Loc, Error> = Result<(Loc, Token, Loc), Error>;
@@ -41,24 +43,148 @@ pub enum Token<'input> {
     IO_NUMBER(&'input str),
 }
 
-pub enum Error {}
+#[derive(Debug)]
+pub enum Error {
+    UnrecognizedChar(usize, char, usize),
+}
 
+// TODO could technically make EOF a token so we don't need to do Result<Option> shinengans
 pub struct Lexer<'input> {
+    input: &'input str,
     chars: CharIndices<'input>,
+    lookahead: Option<(usize, char, usize)>,
 }
 
 impl<'input> Lexer<'input> {
     pub fn new(input: &'input str) -> Self {
+        let mut chars = input.char_indices();
+        let next = chars.next();
+        let lookahead = next.map(|n| (n.0, n.1, n.0 + n.1.len_utf8()));
         Lexer {
-            chars: input.char_indices(),
+            input,
+            chars,
+            lookahead,
         }
+    }
+
+    fn advance(&mut self) -> Option<(usize, char, usize)> {
+        match self.lookahead {
+            Some((start, ch, end)) => {
+                let next = self.chars.next();
+                self.lookahead = next.map(|n| (n.0, n.1, n.0 + n.1.len_utf8()));
+                Some((start, ch, end))
+            },
+            // EOF case
+            None => None,
+        }
+    }
+
+    fn keyword(
+        &mut self,
+        start: usize,
+        end: usize,
+    ) -> Result<(usize, Token<'input>, usize), Error> {
+        let (word, end) = self.take_while(start, end, is_word_continue);
+        let token = match word {
+            "if" => Token::IF,
+            "then" => Token::THEN,
+            "else" => Token::ELSE,
+            "elif" => Token::ELIF,
+            "fi" => Token::FI,
+            "do" => Token::DO,
+            "done" => Token::DONE,
+            "case" => Token::CASE,
+            "esac" => Token::ESAC,
+            "while" => Token::WHILE,
+            "until" => Token::UNTIL,
+            "for" => Token::FOR,
+            _ => todo!(),
+        };
+        Ok((start, token, end))
+    }
+
+    // TODO escape characters
+    fn single_quote(
+        &mut self,
+        start: usize,
+        end: usize,
+    ) -> Result<(usize, Token<'input>, usize), Error> {
+        let (_, end) = self.take_while(start, end, |ch| ch != '\'');
+        self.advance();
+        Ok((start, Token::WORD(&self.input[start + 1..end]), end))
+    }
+
+    fn double_quote(
+        &mut self,
+        start: usize,
+        end: usize,
+    ) -> Result<(usize, Token<'input>, usize), Error> {
+        let (_, end) = self.take_while(start, end, |ch| ch != '"');
+        self.advance();
+        Ok((start, Token::WORD(&self.input[start + 1..end]), end))
+    }
+
+    // utils for reading until condition is met
+    fn take_until<F>(
+        &mut self,
+        start: usize,
+        mut end: usize,
+        mut terminate: F,
+    ) -> (&'input str, usize)
+    where
+        F: FnMut(char) -> bool,
+    {
+        while let Some((_, ch, _)) = self.lookahead {
+            if terminate(ch) {
+                return (&self.input[start..end], end);
+            } else if let Some((_, _, e)) = self.advance() {
+                end = e;
+            }
+        }
+        (&self.input[start..end], end)
+    }
+
+    fn take_while<F>(&mut self, start: usize, end: usize, mut keep_going: F) -> (&'input str, usize)
+    where
+        F: FnMut(char) -> bool,
+    {
+        self.take_until(start, end, |c| !keep_going(c))
     }
 }
 
 impl<'input> Iterator for Lexer<'input> {
     type Item = Spanned<Token<'input>, usize, Error>;
 
+    // TODO create proc macro to generate all this?
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        while let Some((start, ch, end)) = self.advance() {
+            let token = match ch {
+                '!' => Some(Ok((start, Token::BANG, end))),
+                '\'' => Some(self.single_quote(start, end)),
+                '"' => Some(self.double_quote(start, end)),
+                ch if is_word_start(ch) => Some(self.keyword(start, end)),
+                ch if ch.is_whitespace() => continue,
+                ch => return Some(Err(Error::UnrecognizedChar(start, ch, end))),
+            };
+            return token;
+        }
+        return None;
+    }
+}
+
+/// predicate that detects when a word starts (non whitespace, non control character)
+fn is_word_start(ch: char) -> bool {
+    match ch {
+        '\u{007F}' | '\u{0000}'..='\u{001F}' | '\u{0080}'..='\u{009F}' => false,
+        _ => is_word_continue(ch),
+    }
+}
+
+/// predicate for when to keep reading word token
+fn is_word_continue(ch: char) -> bool {
+    match ch {
+        ';' | ')' | '(' | '`' | '!' | '=' | '\\' | '\'' | '"' | '>' | '<' | '&' | '|' | '{'
+        | '}' | '*' => false,
+        _ => !ch.is_whitespace(),
     }
 }

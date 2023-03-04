6040dbab01bff5dfee32acb43a00584210aab076
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-03-03 22:02:12 -0500

    cursor

--- shrs_line/src/lib.rs
+++ shrs_line/src/lib.rs
@@ -1,11 +1,13 @@
 //! Readline implementation for shrs
+
+#![feature(linked_list_cursors)]
 pub mod completion;
 pub mod prompt;
 
-use std::time::Duration;
+use std::{collections::LinkedList, time::Duration};
 
 use crossterm::{
-    event::{poll, read, Event, KeyCode},
+    event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 use prompt::Prompt;
@@ -35,30 +37,45 @@ impl Line {
 }
 
 pub fn read_events() -> crossterm::Result<()> {
-    let mut point = 0;
-    let mut buf: Vec<u8> = vec![];
+    let mut buf: LinkedList<u8> = LinkedList::new();
+    let mut cursor = buf.cursor_front_mut();
 
     enable_raw_mode()?;
     loop {
         if poll(Duration::from_millis(1000))? {
             let event = read()?;
-            println!("got event {:?}\r", event);
             match event {
-                Event::Key(keycode) => {
-                    // handle special characters
-                    if keycode == KeyCode::Esc.into() {
-                        break;
-                    }
-
-                    if keycode == KeyCode::Left {}
-                    if keycode == KeyCode::Right {}
-
-                    // otherwise we can append inputted character to buffer
+                Event::Key(KeyEvent {
+                    code: KeyCode::Esc,
+                    modifiers: KeyModifiers::NONE,
+                }) => {
+                    break;
+                },
+                Event::Key(KeyEvent {
+                    code: KeyCode::Left,
+                    modifiers: KeyModifiers::NONE,
+                }) => {
+                    cursor.move_prev();
+                },
+                Event::Key(KeyEvent {
+                    code: KeyCode::Right,
+                    modifiers: KeyModifiers::NONE,
+                }) => {
+                    cursor.move_next();
+                },
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char(c),
+                    modifiers: KeyModifiers::NONE,
+                }) => {
+                    cursor.insert_after(c as u8);
                 },
                 _ => {},
             }
+
+            println!("got event {:?}\r", event);
         }
     }
+    println!("buffer {:?}\r", buf);
     disable_raw_mode()?;
     Ok(())
 }

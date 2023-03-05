021ad8b3f3fadf500306af7e01bcf4c68406b002
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-03-04 20:06:08 -0500

    simple completions

--- shrs_line/src/completion.rs
+++ shrs_line/src/completion.rs
@@ -1,3 +1,5 @@
+use trie_rs::{Trie, TrieBuilder};
+
 pub struct Completion {}
 
 // also provide some commonly used completion lists
@@ -6,3 +8,52 @@ pub struct Completion {}
 // - file extension
 // - filename regex
 // - known hosts
+
+pub trait Completer {
+    fn complete(&self, buf: &str, cursor_pos: usize);
+}
+
+pub struct DefaultCompleter {
+    completions: Trie<u8>,
+}
+
+/// Very basic completer that uses prefix tree to match on a predefined word list
+// TODO next step, make word list vary with context
+// TODO differ between cmdname, args etc
+impl DefaultCompleter {
+    pub fn new(wordlist: Vec<&str>) -> Self {
+        // build prefix tree from wordlist
+        let mut builder = TrieBuilder::new();
+        for word in wordlist {
+            builder.push(word);
+        }
+        let trie = builder.build();
+
+        DefaultCompleter { completions: trie }
+    }
+}
+
+impl Completer for DefaultCompleter {
+    fn complete(&self, buf: &str, cursor_pos: usize) {
+        let results = self.completions.predictive_search(buf);
+        let results: Vec<&str> = results
+            .iter()
+            .map(|x| std::str::from_utf8(x).unwrap())
+            .collect();
+
+        println!("{:?}", results);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Completer, DefaultCompleter};
+
+    #[test]
+    fn default_completer() {
+        let completer = DefaultCompleter::new(vec![
+            "ls", "ld", "ldd", "let", "ln", "lsblk", "lscpu", "lspci", "lsusb",
+        ]);
+        completer.complete("ls", 0);
+    }
+}

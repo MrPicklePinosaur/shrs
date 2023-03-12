c86f23dc6afead8ec2abe02d5a49a454b93f0d09
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-03-12 19:48:14 -0400

    flags for export

--- shrs_lib/src/builtin/export.rs
+++ shrs_lib/src/builtin/export.rs
@@ -11,6 +11,10 @@ use crate::shell::{dummy_child, Context, Runtime, Shell};
 #[derive(Parser)]
 struct Cli {
     vars: Vec<String>,
+    #[arg(short)]
+    p: bool,
+    #[arg(short)]
+    n: bool,
 }
 
 #[derive(Subcommand)]
@@ -27,6 +31,22 @@ impl BuiltinCmd for ExportBuiltin {
         args: &Vec<String>,
     ) -> anyhow::Result<Child> {
         let cli = Cli::parse_from(vec!["export".to_string()].iter().chain(args.iter()));
+
+        // remove arg
+        if cli.n {
+            for var in cli.vars {
+                rt.env.remove(&var);
+            }
+            return dummy_child();
+        }
+
+        // print all env vars
+        if cli.p {
+            for (var, val) in rt.env.all().iter() {
+                println!("export {}={}", var, val);
+            }
+            return dummy_child();
+        }
 
         for var in cli.vars {
             let mut it = var.splitn(2, "=");

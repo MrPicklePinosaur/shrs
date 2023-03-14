0fd03c54b682cfb6d8ca9c10876cb25167fc2ab9
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-03-14 00:16:17 -0400

    take readline as context param

--- shrs_lib/examples/simple.rs
+++ shrs_lib/examples/simple.rs
@@ -7,9 +7,18 @@ use shrs::{
     prompt::{hostname, top_pwd, username},
     shell::{self, Context, Runtime},
 };
+use shrs_line::line::Line;
 
 fn main() {
     use shell::Shell;
+
+    let completions: Vec<String> = find_executables_in_path(rt.env.get("PATH").unwrap());
+    let completer = shrs_line::completion::DefaultCompleter::new(completions);
+    let menu = shrs_line::menu::DefaultMenu::new();
+    let history = shrs_line::history::DefaultHistory::new();
+    let cursor = shrs_line::cursor::DefaultCursor::default();
+
+    let readline = Line::new(menu, completer, history, cursor);
 
     let myshell = Shell {
         ..Default::default()
@@ -18,6 +27,7 @@ fn main() {
     let mut alias = Alias::new();
     alias.set("ls", "ls -al");
     let mut ctx = Context {
+        readline,
         alias,
         ..Default::default()
     };

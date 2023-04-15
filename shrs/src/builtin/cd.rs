93f95ec2a7009db0afb09964b745206854f8b237
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-04-15 19:38:50 +0900

    cd command hook (#71)
    * cd command hook
    
    * working cd hook

--- shrs/src/builtin/cd.rs
+++ shrs/src/builtin/cd.rs
@@ -5,6 +5,7 @@ use std::{
 
 use super::BuiltinCmd;
 use crate::{
+    hooks::ChangeDirCtx,
     shell::{dummy_child, Context, Runtime},
     Shell,
 };
@@ -37,10 +38,18 @@ impl BuiltinCmd for CdBuiltin {
             Path::new(home_dir).to_path_buf()
         };
 
-        rt.env
-            .set("OLDPWD", &env::current_dir().unwrap().display().to_string());
+        let old_pwd = env::current_dir().unwrap();
+        rt.env.set("OLDPWD", &old_pwd.display().to_string());
 
         env::set_current_dir(path.clone())?; // TODO should env current dir remain as the directory the shell was started in?
+
+        let hook_ctx = ChangeDirCtx {
+            old_dir: old_pwd,
+            new_dir: path.clone(),
+        };
+        // need to be able to call hook from here
+        sh.hooks.change_dir.run(sh, ctx, rt, &hook_ctx);
+
         rt.working_dir = path.clone();
         rt.env.set("PWD", path.to_str().unwrap());
 

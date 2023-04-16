8b85e8aab76bc192e6ed200e5b4b36231ef8a817
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-04-16 14:02:12 +0900

    job handling system (#74)
    * job handling system
    
    * retain jobs

---
+++ shrs/src/jobs.rs
@@ -0,0 +1,71 @@
+//! Abstraction layer for processes
+use std::{
+    collections::{hash_map::Iter, HashMap},
+    process::Child,
+};
+
+use pino_deref::Deref;
+
+pub type JobId = u32;
+
+#[derive(Deref)]
+pub struct ExitStatus(pub i32);
+
+impl ExitStatus {
+    pub fn success(&self) -> bool {
+        self.0 == 0
+    }
+    pub fn code(&self) -> i32 {
+        self.0
+    }
+}
+
+/// Keeps track of all the current running jobs
+pub struct Jobs {
+    next_id: JobId,
+    jobs: HashMap<JobId, Child>,
+}
+
+impl Jobs {
+    pub fn new() -> Self {
+        Jobs {
+            next_id: 0,
+            jobs: HashMap::new(),
+        }
+    }
+
+    /// Add new job to be tracked
+    pub fn push(&mut self, child: Child) {
+        let next_id = self.get_next_id();
+        self.jobs.insert(next_id, child);
+    }
+
+    pub fn iter(&self) -> Iter<'_, JobId, Child> {
+        self.jobs.iter()
+    }
+
+    /// Clean up finished jobs
+    pub fn retain(&mut self, exit_handler: fn(status: ExitStatus)) {
+        self.jobs.retain(|k, v| {
+            match v.try_wait() {
+                Ok(Some(status)) => {
+                    exit_handler(ExitStatus(status.code().unwrap()));
+                    false
+                },
+                Ok(None) => true,
+                Err(e) => {
+                    // TODO should throw error that there was issue waiting for job to finish
+                    false
+                },
+            }
+        });
+    }
+
+    /// Increment internally used id and get the next avaliable one
+    ///
+    /// Careful of overflow!
+    fn get_next_id(&mut self) -> JobId {
+        self.next_id += 1;
+        self.next_id
+    }
+}

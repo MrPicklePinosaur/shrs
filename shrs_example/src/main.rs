4bb1df2a34cb4faffa130be9f41dae560690d814
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-05-23 01:23:18 +0900

    alias support

--- shrs_example/src/main.rs
+++ shrs_example/src/main.rs
@@ -7,6 +7,7 @@ use std::{
 use shrs::prelude::*;
 use shrs_cd_tools::git;
 use shrs_command_timer::{CommandTimerPlugin, CommandTimerState};
+use shrs_lang_nu::NuLangPlugin;
 use shrs_output_capture::OutputCapturePlugin;
 use shrs_run_context::RunContextPlugin;
 
@@ -146,6 +147,7 @@ a rusty POSIX shell | build {}"#,
         .with_plugin(OutputCapturePlugin)
         .with_plugin(CommandTimerPlugin)
         .with_plugin(RunContextPlugin)
+        .with_plugin(NuLangPlugin)
         .build()
         .unwrap();
 

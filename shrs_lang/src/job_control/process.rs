580563130fb9a32cbe2439b2a1ac94b88e2d921b
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-05-30 00:15:07 +0900

    more process

--- shrs_lang/src/job_control/process.rs
+++ shrs_lang/src/job_control/process.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt,
+    fmt, iter,
     process::{Child, ChildStdout, Command, ExitStatus, Stdio},
 };
 
@@ -7,6 +7,12 @@ use super::io::Stdin;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct ProcessId(u32);
+
+impl From<u32> for ProcessId {
+    fn from(value: u32) -> Self {
+        ProcessId(value)
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ProcessStatus {
@@ -43,4 +49,135 @@ pub struct ProcessGroup {
     pub id: Option<u32>,
     pub processes: Vec<Box<dyn Process>>,
     pub foreground: bool,
+}
+
+struct BuiltinProcess {
+    argv: Vec<String>,
+    status_code: ExitStatus,
+    stdout: Option<Stdin>,
+}
+
+impl BuiltinProcess {
+    pub fn new<S1, S2>(
+        program: S1,
+        args: &[S2],
+        status_code: ExitStatus,
+        stdout: Option<Stdin>,
+    ) -> Self
+    where
+        S1: AsRef<str>,
+        S2: AsRef<str>,
+    {
+        Self {
+            argv: iter::once(program)
+                .map(|p| p.as_ref().to_string())
+                .chain(args.iter().map(|arg| arg.as_ref().to_string()))
+                .collect(),
+            status_code,
+            stdout,
+        }
+    }
+}
+
+impl Process for BuiltinProcess {
+    fn id(&self) -> Option<ProcessId> {
+        None
+    }
+
+    fn argv(&self) -> String {
+        self.argv[..].join(" ")
+    }
+
+    fn status(&self) -> ProcessStatus {
+        ProcessStatus::Completed
+    }
+
+    fn status_code(&self) -> Option<ExitStatus> {
+        Some(self.status_code)
+    }
+
+    fn stdout(&mut self) -> Option<Stdin> {
+        self.stdout.take()
+    }
+
+    fn kill(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn wait(&mut self) -> anyhow::Result<ExitStatus> {
+        Ok(self.status_code)
+    }
+
+    fn try_wait(&mut self) -> anyhow::Result<Option<ExitStatus>> {
+        Ok(Some(self.status_code))
+    }
+}
+
+struct ExternalProcess {
+    argv: Vec<String>,
+    child: Child,
+    status: ProcessStatus,
+    status_code: Option<ExitStatus>,
+}
+
+impl ExternalProcess {
+    pub fn new<S1, S2>(program: S1, args: &[S2], child: Child) -> Self
+    where
+        S1: AsRef<str>,
+        S2: AsRef<str>,
+    {
+        Self {
+            argv: iter::once(&program)
+                .map(|p| p.as_ref().to_string())
+                .chain(args.iter().map(|arg| arg.as_ref().to_string()))
+                .collect(),
+            child,
+            status: ProcessStatus::Running,
+            status_code: None,
+        }
+    }
+}
+
+impl Process for ExternalProcess {
+    fn id(&self) -> Option<ProcessId> {
+        Some(self.child.id().into())
+    }
+
+    fn argv(&self) -> String {
+        self.argv[..].join(" ")
+    }
+
+    fn status(&self) -> ProcessStatus {
+        self.status
+    }
+
+    fn status_code(&self) -> Option<ExitStatus> {
+        self.status_code
+    }
+
+    fn stdout(&mut self) -> Option<Stdin> {
+        self.child.stdout.take().map(Stdin::Child)
+    }
+
+    fn kill(&mut self) -> anyhow::Result<()> {
+        self.child.kill()?;
+        Ok(())
+    }
+
+    fn wait(&mut self) -> anyhow::Result<ExitStatus> {
+        let exit_status = self.child.wait()?;
+        self.status = ProcessStatus::Completed;
+        self.status_code = Some(exit_status);
+        Ok(exit_status)
+    }
+
+    fn try_wait(&mut self) -> anyhow::Result<Option<ExitStatus>> {
+        if let Some(exit_status) = self.child.try_wait()? {
+            self.status = ProcessStatus::Completed;
+            self.status_code = Some(exit_status);
+            Ok(Some(exit_status))
+        } else {
+            Ok(None)
+        }
+    }
 }

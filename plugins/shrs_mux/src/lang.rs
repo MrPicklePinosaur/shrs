a7c37464c84a806a4b73a601203ec9f2f12432f0
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-11-01 00:14:42 -0400

    reading from stdout and stdin

--- plugins/shrs_mux/src/lang.rs
+++ plugins/shrs_mux/src/lang.rs
@@ -1,7 +1,9 @@
 use std::{
+    cell::RefCell,
     collections::HashMap,
-    io::Read,
-    process::{Command, ExitStatus, Stdio},
+    io::{BufRead, BufReader, Read, Write},
+    ops::Add,
+    process::{Child, Command, ExitStatus, Stdio},
 };
 
 use shrs::prelude::*;
@@ -119,11 +121,22 @@ impl Lang for PythonLang {
     }
 }
 
-pub struct BashLang {}
+pub struct BashLang {
+    pub instance: RefCell<Child>,
+}
 
 impl BashLang {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            instance: RefCell::new(
+                Command::new("bash")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .spawn()
+                    .expect("Failed to start bash lol"),
+            ),
+        }
     }
 }
 
@@ -135,14 +148,55 @@ impl Lang for BashLang {
         rt: &mut Runtime,
         cmd: String,
     ) -> shrs::anyhow::Result<CmdOutput> {
-        let mut handle = Command::new("bash")
-            .args(vec!["-c", &cmd])
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()?;
-        let output = handle.wait_with_output()?;
+        let mut instance = self.instance.borrow_mut();
+        let stdin = instance.stdin.as_mut().expect("Failed to open stdin");
+        // stdin
+        //     .write_all(cmd.as_bytes())
+        //     .expect("Failed to write to stdin");
 
-        Ok(CmdOutput::from(output))
+        //idk how to echo to stdout and stderr
+        stdin
+            .write_all((cmd + ";echo '\x1A'; echo '\x1A' >&2\n").as_bytes())
+            .expect("Failed to send Ctrl+C to stdin");
+
+        let mut stdout_reader =
+            BufReader::new(instance.stdout.as_mut().expect("Failed to open stdout"));
+
+        let mut stdout = String::new();
+        loop {
+            let mut line = String::new();
+
+            stdout_reader.read_line(&mut line)?;
+            dbg!(line.clone());
+
+            if line.contains("\x1a") {
+                break;
+            }
+
+            stdout.push_str(line.as_str());
+        }
+
+        let mut stderr_reader =
+            BufReader::new(instance.stderr.as_mut().expect("Failed to open stdout"));
+
+        let mut stderr = String::new();
+        loop {
+            let mut line = String::new();
+
+            stderr_reader.read_line(&mut line)?;
+
+            dbg!(line.clone());
+
+            if line.contains("\x1a") {
+                break;
+            }
+
+            stderr.push_str(line.as_str());
+        }
+
+        // println!("{}", stdout.trim_end_matches("\n"));
+
+        Ok(CmdOutput::empty())
     }
 
     fn name(&self) -> String {

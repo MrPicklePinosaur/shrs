2b2a657b099f627baaabeaa26d71e632d4aa52f4
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-11-08 16:35:09 -0500

    analytics plugin

---
+++ plugins/shrs_analytics/src/lib.rs
@@ -0,0 +1,9 @@
+use shrs::prelude::*;
+
+pub struct AnalyticsPlugin;
+
+impl Plugin for AnalyticsPlugin {
+    fn init(&self, shell: &mut ShellConfig) -> anyhow::Result<()> {
+        Ok(())
+    }
+}

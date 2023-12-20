318ed360dba252f4547e65b19f8f21baf96b497e
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-20 01:36:17 -0500

    add commonly used dirs

--- plugins/shrs_analytics/src/builtin.rs
+++ plugins/shrs_analytics/src/builtin.rs
@@ -14,12 +14,23 @@ impl BuiltinCmd for AnalyticsBuiltin {
     ) -> anyhow::Result<CmdOutput> {
         //Args, timeframe; This session or all time
         //which metric
-        ctx.out.println("Analytics for ")?;
+        const LIMIT: usize = 5;
+
         ctx.state.get::<AnalyticsState>().map(|state| {
+            ctx.out.println("most used commands ====").unwrap();
             let mut commands = state.commands.iter().collect::<Vec<_>>();
             commands.sort_by(|a, b| b.cmp(a));
-            for (cmd, count) in commands.iter() {
+            for (cmd, count) in commands.iter().take(LIMIT) {
                 let out = format!("{cmd} {count}");
+                // TODO would like ctx.out.println to be a macro like println!
+                ctx.out.println(out).unwrap();
+            }
+
+            ctx.out.println("most used dirs ====").unwrap();
+            let mut dirs = state.dirs.iter().collect::<Vec<_>>();
+            dirs.sort_by(|a, b| b.cmp(a));
+            for (dir, count) in dirs.iter().take(LIMIT) {
+                let out = format!("{dir:?} {count}");
                 // TODO would like ctx.out.println to be a macro like println!
                 ctx.out.println(out).unwrap();
             }

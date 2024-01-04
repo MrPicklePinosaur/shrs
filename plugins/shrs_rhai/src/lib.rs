8b9c92af644de9e5334f9194b3d4d3429cbde030
Daniel Liu <mr.picklepinosaur@gmail.com>
2024-01-04 15:27:46 -0800

    call registered rhai functions from shell (#325)
    * call registered rhai functions from shell
    
    * use rhai state
    
    * store ast of each script
    
    * add command after hook to plugin
    
    * working function calling

--- plugins/shrs_rhai/src/lib.rs
+++ plugins/shrs_rhai/src/lib.rs
@@ -1,6 +1,8 @@
 mod builtin;
 mod rhai;
 
+use builtin::command_not_found_hook;
+use rhai::RhaiState;
 use shrs::prelude::*;
 
 pub struct RhaiPlugin;
@@ -8,6 +10,13 @@ pub struct RhaiPlugin;
 impl Plugin for RhaiPlugin {
     fn init(&self, shell: &mut ShellConfig) -> anyhow::Result<()> {
         shell.builtins.insert("source", builtin::RhaiBuiltin::new());
+        shell.hooks.insert(command_not_found_hook);
+        Ok(())
+    }
+
+    fn post_init(&self, sh: &Shell, ctx: &mut Context, rt: &mut Runtime) -> anyhow::Result<()> {
+        let state = RhaiState::new(sh, ctx, rt);
+        ctx.state.insert(state);
         Ok(())
     }
 }

6706b5ab115dc1e81451222b1d5aecbed1351f5c
Daniel Liu <mr.picklepinosaur@gmail.com>
2024-01-17 20:50:44 -0800

    ssh mux lang

--- plugins/shrs_mux/examples/basic.rs
+++ plugins/shrs_mux/examples/basic.rs
@@ -1,11 +1,12 @@
 use shrs::prelude::*;
-use shrs_mux::{BashLang, MuxPlugin, NuLang, PythonLang};
+use shrs_mux::{BashLang, MuxPlugin, NuLang, PythonLang, SshLang};
 
 fn main() {
     let mux_plugin = MuxPlugin::new()
         .register_lang("bash", BashLang::new())
         .register_lang("python", PythonLang::new())
-        .register_lang("nu", NuLang::new());
+        .register_lang("nu", NuLang::new())
+        .register_lang("ssh", SshLang::new());
 
     let myshell = ShellBuilder::default()
         .with_plugin(mux_plugin)
---
+++ plugins/shrs_mux/src/lang/ssh.rs
@@ -0,0 +1,107 @@
+use std::{process::Stdio, sync::Arc};
+
+use shrs::prelude::*;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    process::{Child, ChildStdin, ChildStdout, Command},
+    runtime,
+    sync::{
+        mpsc::{self, Sender},
+        RwLock,
+    },
+};
+
+use crate::{
+    interpreter::{read_err, read_out},
+    MuxState,
+};
+
+// TODO this is just a copy pasted version of PythonLang, abstract in future
+
+pub struct SshLang {
+    instance: Child,
+    /// Channel for writing to process
+    write_tx: Sender<String>,
+    runtime: runtime::Runtime,
+}
+
+impl SshLang {
+    pub fn new() -> Self {
+        let runtime = runtime::Runtime::new().unwrap();
+
+        let _guard = runtime.enter();
+
+        // TODO maybe support custom parameters to pass to command
+        let args = vec!["-tt", "website@danieliu.xyz"];
+        let mut instance = Command::new("ssh")
+            .args(args)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("Failed to start ssh process");
+
+        let stdout = instance.stdout.take().unwrap();
+        let stderr = instance.stderr.take().unwrap();
+        let stdin = instance.stdin.take().unwrap();
+
+        runtime.spawn(async {
+            let mut stdout_reader = BufReader::new(stdout).lines();
+            while let Some(line) = stdout_reader.next_line().await.unwrap() {
+                println!("{line}");
+            }
+        });
+
+        runtime.spawn(async {
+            let mut stderr_reader = BufReader::new(stderr).lines();
+            while let Some(line) = stderr_reader.next_line().await.unwrap() {
+                eprintln!("{line}");
+            }
+        });
+
+        let (write_tx, mut write_rx) = mpsc::channel::<String>(8);
+
+        runtime.spawn(async move {
+            let mut stdin_writer = BufWriter::new(stdin);
+
+            while let Some(cmd) = write_rx.recv().await {
+                stdin_writer
+                    .write_all((cmd + "\n").as_bytes())
+                    .await
+                    .expect("ssh command failed");
+
+                stdin_writer.flush().await.unwrap();
+            }
+        });
+
+        Self {
+            instance,
+            write_tx,
+            runtime,
+        }
+    }
+}
+
+impl Lang for SshLang {
+    fn eval(
+        &self,
+        sh: &Shell,
+        ctx: &mut Context,
+        rt: &mut Runtime,
+        cmd: String,
+    ) -> shrs::anyhow::Result<CmdOutput> {
+        self.runtime.block_on(async {
+            self.write_tx.send(cmd).await.unwrap();
+        });
+
+        Ok(CmdOutput::success())
+    }
+
+    fn name(&self) -> String {
+        "ssh".to_string()
+    }
+
+    fn needs_line_check(&self, cmd: String) -> bool {
+        false
+    }
+}

660728eee733db4775867b251f2e3691d11fbc1c
Daniel Liu <mr.picklepinosaur@gmail.com>
2024-01-25 22:20:50 -0800

    poc openssh crate

--- plugins/shrs_mux/src/lang/ssh.rs
+++ plugins/shrs_mux/src/lang/ssh.rs
@@ -1,16 +1,14 @@
 use std::{
     cell::RefCell,
     env,
-    io::{BufRead, BufReader, BufWriter, Write},
     net::TcpStream,
-    process::Stdio,
     sync::{Arc, OnceLock},
 };
 
+use openssh::{Child, KnownHosts, Session, Stdio};
 use shrs::prelude::*;
-use ssh2::{Session, Stream};
 use tokio::{
-    process::{Child, ChildStdin, ChildStdout, Command},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
     runtime,
     sync::{
         mpsc::{self, Sender},
@@ -24,50 +22,47 @@ use crate::{
 };
 
 struct SshLangCtx {
-    stdin_writer: RefCell<BufWriter<Stream>>,
+    session: Arc<Session>,
+    shell: Child<Arc<Session>>,
 }
 
 impl SshLangCtx {
     // TODO kinda ugly to be passing remote through the original SshLang too
     fn init(runtime: &runtime::Runtime, remote: &str) -> Self {
-        // TODO just use env vars for auth for now
-        let address = env::var("SHRS_SSH_ADDRESS").unwrap();
-        let username = env::var("SHRS_SSH_USERNAME").unwrap();
-        let password = env::var("SHRS_SSH_PASSWORD").unwrap();
-
-        let tcp = TcpStream::connect(address).unwrap();
-        let mut session = Session::new().unwrap();
-        session.set_tcp_stream(tcp);
-        session.handshake().unwrap();
-        session.userauth_password(&username, &password).unwrap();
-        // TODO we can implement an interactive password prompt
-        // session.userauth_keyboard_interactive();;
-
-        println!("successful auth");
-
-        let mut channel = session.channel_session().unwrap();
-        channel.request_pty("xterm", None, None).unwrap();
-        channel.shell().unwrap();
-        session.set_blocking(false);
-        // channel.exec("ls -al").unwrap();
-
         let _guard = runtime.enter();
 
-        let stdin = channel.stream(0);
-        let stdout = channel.stream(0);
-        let stderr = channel.stderr();
+        let ctx = runtime.block_on(async {
+            let session =
+                Session::connect(env::var("SHRS_SSH_ADDRESS").unwrap(), KnownHosts::Strict)
+                    .await
+                    .unwrap();
+            let session = Arc::new(session);
 
-        runtime.spawn(async {
-            let mut stdout_reader = BufReader::new(stdout).lines();
-            while let Some(Ok(line)) = stdout_reader.next() {
-                println!("{line}");
-            }
-            eprintln!("EXITED");
+            println!("connected to server");
+
+            let mut shell = session
+                .clone()
+                .arc_command("sh")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .await
+                .unwrap();
+
+            let stdout = shell.stdout().take().unwrap();
+
+            runtime.spawn(async {
+                let mut stdout_reader = BufReader::new(stdout).lines();
+                while let Some(line) = stdout_reader.next_line().await.unwrap() {
+                    println!("{line}");
+                }
+            });
+
+            SshLangCtx { session, shell }
         });
 
-        SshLangCtx {
-            stdin_writer: RefCell::new(BufWriter::new(stdin)),
-        }
+        ctx
     }
 }
 
@@ -100,14 +95,6 @@ impl Lang for SshLang {
         let lang_ctx = self
             .lang_ctx
             .get_or_init(|| SshLangCtx::init(&self.runtime, &self.remote));
-
-        // self.runtime.block_on(async {});
-        let mut stdin_writer = lang_ctx.stdin_writer.borrow_mut();
-        stdin_writer
-            .write_all((cmd.clone() + "\n").as_bytes())
-            .unwrap();
-        stdin_writer.flush().unwrap();
-        println!("wrote {cmd}");
 
         Ok(CmdOutput::success())
     }
